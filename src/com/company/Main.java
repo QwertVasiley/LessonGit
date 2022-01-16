18515dba47920bbceb3e38b56cf1c4c68544195c
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-01-16 20:27:57 +0300

    First lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("In first number: ");
+        int first = scanner.nextInt();
+        System.out.print("In second number: ");
+        int second = scanner.nextInt();
+        System.out.print("In third number: ");
+        int third = scanner.nextInt();
+
+
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
     }
 }
