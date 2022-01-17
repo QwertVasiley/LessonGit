afac25c47944ef344218cdf2dd93476a27b7ab18
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-01-17 19:13:20 +0300

    First lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+// Пользователь вводит 3 числа. Найти максимальное и вывести его
+// Способ 1
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("In first number: ");
         int first = scanner.nextInt();
@@ -26,6 +29,58 @@ public class Main {
         {
             System.out.println("Большее число : " +third);
         }
+        if (first > second && second < third & third > first)
+        {
+            System.out.println("Большее число : " +third);
+        }
+        if (first == second && second == third)
+        {
+            System.out.println("Все числа одинаковы и равны :" +first);
+        }
+        if (first == second && second > third)
+        {
+            System.out.println("Первые и второе число имеют одинаковое значение " +first +" и больше последнего");
+        }
+        if (first < second && second == third)
+        {
+            System.out.println("Два последних числа имеют одинаковое значение " +third +" и больше первого");
+        }
+        if (first>second && first == third)
+        {
+            System.out.println("Первое и третье число имеют одинаковое значение " +first + " и больше второго");
+        }
+        if (first == second && second < third)
+        {
+            System.out.println("Большее число : " +third);
+        }
+        if (first > second && second == third)
+        {
+            System.out.println("Большее число : " +first);
+        }
         else {}
     }
 }
+
+        // Второй способ
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("In first number: ");
+//        int first = scanner.nextInt();
+//        System.out.print("In second number: ");
+//        int second = scanner.nextInt();
+//        System.out.print("In third number: ");
+//        int third = scanner.nextInt();
+//
+//        if (first > second && second > third) System.out.println("Большее число : " +first);
+//        if (first < second && second > third) System.out.println("Большее число : " +second);
+//        if (first < second && second < third) System.out.println("Большее число : " +third);
+//        if (first > second && second < third & third > first) System.out.println("Большее число : " +third);
+//        if (first == second && second == third) System.out.println("Все числа одинаковы и равны " +first);
+//        if (first == second && second > third) System.out.println("Первые и второе число имеют одинаковое значение " +first +" и больше последнего");
+//        if (first < second && second == third) System.out.println("Два последних числа имеют одинаковое значение " +third +" и больше первого");
+//        if (first>second && first == third) System.out.println("Первое и третье число имеют одинаковое значение " +first + " и больше второго");
+//        if (first == second && second < third) System.out.println("Большее число : " +third);
+//        if (first > second && second == third) System.out.println("Большее число : " +first);
+//
+//    }
+//}
