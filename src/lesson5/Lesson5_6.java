c90bf8dbae0e2fcd6ef449e47e00bc9a1e80153e
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 22:07:42 +0300

    Update Lesson5_6.java

--- src/lesson5/Lesson5_6.java
+++ src/lesson5/Lesson5_6.java
@@ -4,12 +4,13 @@ public class Lesson5_6 {
     public static void main(String[] args) {
 
         // Вычислить: 1+2+4+8+...+256
-
+//what is b?? naming!
         int b=1;
         int sum = 1;
         while (b < 256) {
-
+            //b++
             b = b+b; //создаёт ряд чисел нужных для сложения
+            //sum+=b
             sum=sum+b;
 
         }
