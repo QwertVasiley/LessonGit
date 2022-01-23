1e9ace407c898c1bf0547db31ab2bf3415a64cf3
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-01-23 10:20:30 +0300

    First lesson

--- src/lesson5/Lesson5_1.java
+++ src/lesson5/Lesson5_1.java
@@ -2,11 +2,9 @@ package lesson5;
 
 public class Lesson5_1 {
     public static void main(String[] args) {
-//        for (int a=1; a<=10 ; a++)
-//            System.out.println(a);
-//
-//    }
-//}
+        for (int a=1; a<=10 ; a++)
+            System.out.println(a);
+
 
     }}
 
