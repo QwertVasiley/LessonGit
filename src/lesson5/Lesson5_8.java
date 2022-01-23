aa2c043d939a44ea06bde5dd4bc64cf6d0e2f73f
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 22:09:40 +0300

    Update Lesson5_8.java

--- src/lesson5/Lesson5_8.java
+++ src/lesson5/Lesson5_8.java
@@ -1,12 +1,13 @@
 package lesson5;
 
     //Напишите программу, определяющую сумму всех нечетных чисел от 1 до 99
-
+//+
 public class Lesson5_8 {
     public static void main(String[] args) {
         int i=1;
         int sum = 1;
         while (i<99) {
+            //i+=2, sum+=i
             i=i+2;
             sum = sum+i;
 
