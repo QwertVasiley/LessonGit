58c01d56275d46245a90a497bd173a37af056a9b
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-01-30 11:56:45 +0300

    First lesson

---
+++ src/lesson5/Examination.java
@@ -0,0 +1,28 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class Examination {
+    public static void main(String[] args) {
+                //	Посчитать сумму четных, нечетных и всех чисел
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int chet = 0;
+        int sumChet = 0;
+        int noChet;
+        int sumNoChet = 0;
+        int number = scanner.nextInt();
+        while (chet<number) {
+            chet=chet+2;
+            sumChet = sumChet + chet;
+        }
+            for (noChet = 1; noChet<=number; noChet =noChet+2){
+                sumNoChet=sumNoChet+noChet;
+        }
+        System.out.println("Sum of even numbers: "+sumChet);
+        System.out.println("Sum of odd numbers: "+sumNoChet );
+        System.out.println("Total amount: "+(sumChet+sumNoChet));
+
+        }
+    }
+
---
+++ src/lesson5/Examination1.java
@@ -0,0 +1,44 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class Examination1 {
+    public static void main(String[] args) {
+        //Реализуйте калькулятор с возможностью
+        // постоянного ввода числа («Хотите продолжить?»)
+        Scanner scanner = new Scanner(System.in);
+        boolean yesNo = true;
+        do {
+            System.out.println("Input first number: ");
+            int first = scanner.nextInt();
+            System.out.println("Input second number: ");
+            int second = scanner.nextInt();
+            System.out.println("Input operatoin: ");
+            char operation = scanner.next().charAt(0);
+            int result = 0;
+            switch (operation) {
+                case '+':
+                    result = first + second;
+                    break;
+                case '-':
+                    result = first - second;
+                    break;
+                case '*':
+                    result = first * second;
+                    break;
+                case '/':
+                    result = first / second;
+                    break;
+                default:
+            }
+            System.out.println("Result: " + result);
+            System.out.println("Input yes or no to repeat");
+            String prover = scanner.next();
+            if (prover.equals("yes") )
+                yesNo = true;
+            if (prover.equals("no") )
+                yesNo = false;
+            }while (yesNo==true);
+        }
+    }
+
