69eb15a312eb2e3ce270d0f1f2a1f02607c17303
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-04 12:05:32 +0300

    First lesson

--- src/lesson5/Lesson5_9.java
+++ src/lesson5/Lesson5_9.java
@@ -8,24 +8,25 @@ public class Lesson5_9 {
         //Найти общую сумму всех чисел
 
 
-        int c=0; //переменная четных чисел
+        int c = 0; //переменная четных чисел
         int sumChet = 0; //сумма сумм четных чисел
-        int i=1; //переменная не четных чисел
+        int i = 1; //переменная не четных чисел
         int sumNoChet = 1;
 
-        while (c<25){
+        while (c < 25) {
 //you did a lot of operations. we could find odd and even number in 1 while loop using if-else and '%'
-        while (c<100){
-            c=c+2;
-            sumChet = sumChet+c;
-        }
+            while (c < 100) {
+                c = c + 2;
+                sumChet = sumChet + c;
+            }
 
-        while (i<24) {
-            i=i+2;
-            sumNoChet=sumNoChet+i;
+            while (i < 24) {
+                i = i + 2;
+                sumNoChet = sumNoChet + i;
+            }
+            System.out.println("Сумма четных чисел: " + sumChet);
+            System.out.println("Сумма не четных чисел: " + sumNoChet);
+            System.out.println("Общаяя сумма: " + (sumChet + sumNoChet));
         }
-        System.out.println("Сумма четных чисел: "+sumChet);
-        System.out.println("Сумма не четных чисел: "+sumNoChet );
-        System.out.println("Общаяя сумма: "+(sumChet+sumNoChet));
     }
 }
--- src/lesson6/Random.java
+++ src/lesson6/Random.java
@@ -1,9 +1,8 @@
 package lesson6;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Lesson6_2 {
+public class Random {
 
          //RANDOME
 
@@ -11,7 +10,7 @@ public class Lesson6_2 {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите число: ");
             int number = scanner.nextInt();
-            Random random = new Random();
+            java.util.Random random = new java.util.Random();
             int randomNumber = random.nextInt(-100000000, 100000000);
             String text = number < randomNumber ? "меньше" : number > randomNumber ? "больше" : "равно";
             System.out.println("Рандомное число: " + randomNumber);
