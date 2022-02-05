3cd6071019e95d8436c48878c34c80f5181db2d8
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-05 17:55:23 +0300

    First lesson

--- src/array/New_array_even.java
+++ src/array/New_array_even.java
@@ -7,7 +7,7 @@ public class New_array_even {
         // содержащий только четные элементы из первого
         int anArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
         int j = 0; // кол-во ячеек для нового массива
-        int d = 0;
+        int d = 0; // переменная номера ячеек нового массива
         int[] arrayNew;
         for (int i = 0; i < anArray.length; i++) {
             if (anArray[i] % 2 == 0) {
@@ -15,7 +15,7 @@ public class New_array_even {
                 j++;
             }
         }
-        System.out.println("Длинна нового массива " + j);
+        System.out.println("Длинна нового массива равна " + j);
         arrayNew = new int[j]; //установил длинну нового массива
         for (int k = 0; k < anArray.length; k++) {
             if (anArray[k] % 2 == 0) {
