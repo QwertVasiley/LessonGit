033abcf1e1e88b3375f58f9cb29da60ad6efc471
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-11 19:22:01 +0300

    Update sum_diagonal_var2.java

--- src/lesson9/sum_diagonal_var2.java
+++ src/lesson9/sum_diagonal_var2.java
@@ -6,6 +6,7 @@ public class sum_diagonal_var2 {
         int[][] array = {{1, 2, 3, 4}, {4, 3, 2, 1}, {7, 8, 9, 6}, {6, 5, 4, 3}};
         int sum = 0;
         int j = 0;
+        //ok, but could also do it in more "mathematical" way
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i][j];
             j++;
