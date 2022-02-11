fa035bc39ea14b144f3aab532791f3374e060545
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-11 19:20:57 +0300

    Update sum_diagonal.java

--- src/lesson9/sum_diagonal.java
+++ src/lesson9/sum_diagonal.java
@@ -4,6 +4,7 @@ public class sum_diagonal {
     public static void main(String[] args) {
         //Сумма диагонали матрицы
         int[][] array = {{1, 2, 3, 4}, {4, 3, 2, 1}, {7, 8, 9, 6}, {6, 5, 4, 3}};
+        //looks difficult. Could do it with arrays loops and if.
         int sum = 0;
         int i = 0;
         int j = 0;
