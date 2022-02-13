e03d936eb8a54ff6ba486c14297c9ad6bd1ba6a5
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-13 11:34:41 +0300

    First lesson

---
+++ src/Lesson_11/Lesson11_1_1.java
@@ -0,0 +1,24 @@
+package Lesson_11;
+
+public class Lesson11_1_1 {
+    public static void main(String[] args) {
+        //Найти сумму максимальных чисел массива
+        //найти разницу минимальных чисел массива
+        int[] array = {15, 20, 30, 3, 70, 90, 112, 2};
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        for (int output : array) {
+            System.out.print(" " + output);
+        }
+        System.out.println();
+        System.out.println("Min " + (array[0] - array[1]));
+        System.out.println("Max " + (array[array.length - 1] + array[array.length - 2]));
+    }
+}
