93b93aebeae91f846e40afd5fe69f857c71f0382
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-13 11:17:21 +0300

    First lesson

---
+++ src/Lesson_11/Lesson11_1.java
@@ -0,0 +1,35 @@
+package Lesson_11;
+
+public class Lesson11_1 {
+    public static void main(String[] args) {
+        int[] array = {15, 20, 30, 3, 70, 90, 112, 2};
+        int max1 = array[0];
+        int max2 = array[0];
+        int min1 = array[0];
+        int min2 = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max1 < array[i]) {
+                max1 = array[i];
+            }
+        }
+        for (int j = 0; j < array.length; j++) {
+            if (max2 < array[j] && max1 != array[j]) {
+                max2 = array[j];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (min1 > array[i]){
+                min1=array[i];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (min2>array[i] && min1!=array[i]){
+                min2=array[i];
+            }
+        }
+        System.out.println(max1 + " + " + max2 + " = " + (max1 + max2));
+        System.out.println(min1 + " - " + min2 + " = " + (min1-min2));
+    }
+}
+
+
