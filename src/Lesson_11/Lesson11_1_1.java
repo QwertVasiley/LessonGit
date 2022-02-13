4e3e1973663228ca0101e58d527bd3463917e063
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-13 11:44:24 +0300

    First lesson

--- src/Lesson_11/Lesson11_1_1.java
+++ src/Lesson_11/Lesson11_1_1.java
@@ -5,10 +5,11 @@ public class Lesson11_1_1 {
         //Найти сумму максимальных чисел массива
         //найти разницу минимальных чисел массива
         int[] array = {15, 20, 30, 3, 70, 90, 112, 2};
+        int tmp;
         for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
+                    tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
