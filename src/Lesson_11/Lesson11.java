e8b5c87d6d113ad3eaf3eaa45599acc9d50cd853
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-13 10:31:18 +0300

    First lesson

---
+++ src/Lesson_11/Lesson11.java
@@ -0,0 +1,20 @@
+package Lesson_11;
+
+import java.util.Arrays;
+
+public class Lesson11 {
+    public static void main(String[] args) {
+        int[] array = {15, 20, 30, 3, 70, 90, 112, 2};
+        int[] array1 = new int[array.length / 2];
+        int[] array2 = new int[array.length / 2];
+        for (int i = 0; i < array.length; i++) {
+            if (i < array.length / 2) {
+                array1[i] = array[i];
+            }else{
+                array2 [i-array.length/2]= array[i];
+            }
+        }
+        System.out.println("Массив1: " + Arrays.toString(array1));
+        System.out.println("Массив1: " + Arrays.toString(array2));
+    }
+}
