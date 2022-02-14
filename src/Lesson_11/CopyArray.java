c90b84452aa24c9b8faa2ffc76601c1988a6cc90
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-02-14 21:13:50 +0300

    First lesson

--- src/Lesson_11/CopyArray.java
+++ src/Lesson_11/CopyArray.java
@@ -3,11 +3,22 @@ package Lesson_11;
 import java.util.Arrays;
 
 public class CopyArray {
-    //скопировать один массив в другой
+    //скопировать один массив в другой (так делать не правильно)
     public static void main(String[] args) {
         int[] array = {15, 20, 30, 3, 70, 90, 112, 2};
-        int [] arrayNew;
-        arrayNew = array;
-        System.out.println(Arrays.toString(arrayNew));
+
+//        int [] arrayNew;
+//        arrayNew = array;
+//        System.out.println(Arrays.toString(arrayNew));
+//    }
+//}
+
+        //Верный способ
+        int[] array2;
+        array2 = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[i];
+        }
+        System.out.println(Arrays.toString(array2));
     }
 }
