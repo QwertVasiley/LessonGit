560d2e22afbe5af85ceddd9240a25f2d3471ec96
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-04-09 18:20:06 +0300

    First lesson

--- src/Lesson21_2/Main.java
+++ src/Lesson21_2/Main.java
@@ -2,10 +2,12 @@ package Lesson21_2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     //Создать класс «Сотрудник» в нем проинициализировать поля фамилия и отдел.
     // Пометисть это все в мапу
+    // найти сотрудника через сканер
     public static void main(String[] args) {
 
         Employee petrov = new Employee("Petrov", 15);
@@ -17,6 +19,23 @@ public class Main {
         employ.put(sidorov.getLastName(), sidorov.getDepartment());
         employ.put(veselkin.getLastName(), veselkin.getDepartment());
 
-        System.out.println(employ);
+        System.out.println("All departments " + employ);
+
+        System.out.println("Enter LastName ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        for (Map.Entry entry : employ.entrySet()) {
+
+            if (entry.getKey().equals(name)) {
+                System.out.println(name + " " + entry.getValue());
+                return;
+            }
+        }
+        System.out.println("No name");
     }
 }
+
+
+
+
