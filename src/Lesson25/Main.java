9604658474a573050121b1de7b74afd888b8b472
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-05-07 10:17:16 +0300

    First lesson

---
+++ src/Lesson25/Main.java
@@ -0,0 +1,11 @@
+package Lesson25;
+
+public class Main {
+    public static void main(String[] args) {
+            //создать 3 потока
+            //создать матрицу Randome
+            //
+
+
+    }
+}
